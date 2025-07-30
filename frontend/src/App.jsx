5b4d23118ef86e75ade94c98d18df65d67597bb8
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-07-30 22:48:56 +0530

    initialized project and folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='mx-2'>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/AddMembership.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddMembership = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddMembership
