814ba43598163a0997f80fac0ecee419dc521478
dileshwar-01 <rajputdileshwar16@gmail.com>
2025-08-01 22:37:35 +0530

    added linear gradient for background body

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
     
 
   return (
-<form  className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-900 '>
+<form  className='flex flex-col h-screen items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-900 '>
       <div className='inline-flex items-center gap-2 mb-2 mt-10'>
           <p className="text-4xl font-bold mb-6 text-center">{currState}</p>
       </div>
