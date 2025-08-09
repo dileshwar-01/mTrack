89847d4ee164463eaf3b3271f1f9bc306b6639b8
dakshsawhneyy <darkstardaksh@gmail.com>
2025-08-09 03:16:35 +0000

    Added tf outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,17 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_cidr" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "private_subnets" {
+  description = "List of IDs of private subnets"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "List of IDs of public subnets"
+  value       = module.vpc.public_subnets
+}
